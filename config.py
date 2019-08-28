a22f2ad1b4d2d1ae89f4e12bd8a537a621c46735
doublealiu <doublealiu@gmail.com>
2019-08-28 09:56:39 -0700

    Continued work on the configuration section of the bot.

--- config.py
+++ config.py
@@ -1,12 +1,41 @@
 import yaml
+import mysql.connector
+import praw.reddit
+
+data = yaml.safe_load(open("config.yml", 'r'))
+print(data.get("database").get("host"))
+
+#   Represents the state of config.yml at the time that it is called,
+#   nonmutable and will only return data about the config at the time it is called
 
 class Config:
+    def __init__(self, filename):
+        file = open(filename, 'r')
+        self.data = yaml.safe_load(file)
+        file.close()
+
+    def getDatabase(self):
+        dbData = self.data.get("database")
+        return mysql.connector.connect(host = dbData.get("host"),
+                                       database = dbData.get("database-name"),
+                                       user = dbData.get("username"),
+                                       password = dbData.get("password"))
+
+    def getTrackedPosts(self, reddit):
+        reddit = reddit(reddit)
+        idList = self.data.get("tracked-posts")
+        submissions = list()
+        for id in idList:
+            submission = reddit.submission(id=id)
+            if submission is not None:
+                submissions.append(submission)
+        return submissions
+
+    def getReddit(self):
+        redditData = self.data.get("reddit")
+        return praw.Reddit(client_id = redditData.get("client-id"),
+                           client_secret = redditData.get("client-secret"),
+                           user_agent = redditData.get("user-agent"))
+
+class SubredditContext:
     def __init__(self):
-        stream = open("config.yml", "r")
-        if(stream.mode != 'r'):
-            return
-        contents = yaml.load(stream)
-        keys = contents.keys
-        self.subreddits = {}
-        for key, value in keys:
-            self.subreddits[key] = value
