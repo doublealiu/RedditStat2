20317052d1ed112a4f55029253851d14de9c2e4e
quickTwix898 <jacobazhi@gmail.com>
2019-08-29 10:11:17 -0700

    added main function

---
+++ main.py
@@ -0,0 +1,12 @@
+import praw
+from scraper import ScraperMachine
+from scraper import SortType
+import mysql.connector
+
+reddit = praw.Reddit(client_id='0Gw8-EwymQ-fig',
+                     client_secret='Y5kigBf4MmZs-u2m3lh6ZvFjclg',
+                     user_agent='my user agent (test)')
+
+scraper1 = ScraperMachine(redditobject=reddit)
+for x,y in scraper1.listAllSubreddits(type=SortType.HOT, timeframe='all', limit=25).items():
+	print(x,y)
