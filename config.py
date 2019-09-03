e4738c0ebeda0c6fcb0a69f64ba9d8d5f71a8501
doublealiu <doublealiu@gmail.com>
2019-09-03 09:49:22 -0700

    Finished subreddit context part of config.

--- config.py
+++ config.py
@@ -13,10 +13,7 @@ class Config(object):
 
     def getDatabase(self):
         dbData = self.data.get("database")
-        return mysql.connector.connect(host = dbData.get("host"),
-                                       database = dbData.get("database-name"),
-                                       user = dbData.get("username"),
-                                       password = dbData.get("password"))
+        return mysql.connector.connect(host = dbData.get("host"), database = dbData.get("database-name"), user = dbData.get("username"), password = dbData.get("password"))
 
     def getTrackedPosts(self, reddit):
         reddit = reddit(reddit)
@@ -30,15 +27,15 @@ class Config(object):
 
     def getReddit(self):
         redditData = self.data.get("reddit")
-        return praw.Reddit(client_id = redditData.get("client-id"),
-                           client_secret = redditData.get("client-secret"),
-                           user_agent = redditData.get("user-agent"))
+        return praw.Reddit(client_id = redditData.get("client-id"), client_secret = redditData.get("client-secret"), user_agent = redditData.get("user-agent"))
 
     def getsubcontexts(self):
-        subdata = self.data.get("subreddits")
+        subsdata = self.data.get("subreddits")
         subs = list()
-        for sub in subdata.keys:
-            subs.append()
+        for sub in subsdata.keys:
+            datacollect = sub.get("data")
+            singlesub = SubredditContext(sub.get("submissions"), sub.get("timing"), sub.get("sort-by-section"), sub.get("sort-by-time"), datacollect.get("title"), datacollect.get("points"), datacollect.get("self-text"))
+            subs.append(singlesub)
 
 class SubredditContext:
     def __init__(self, subname, submissions, timing, section, time, title, points, selftext):
