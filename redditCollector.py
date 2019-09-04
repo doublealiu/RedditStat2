297b4f772cca88787b5ddeb5221977a616395b63
quickTwix898 <jacobazhi@gmail.com>
2019-09-04 09:56:27 -0700

    Recongigured collector and statistics to be seperate

---
+++ redditCollector.py
@@ -0,0 +1,57 @@
+import praw
+import string
+
+class SortTime():
+    @staticmethod
+    def hour():
+        return "hour"
+    @staticmethod
+    def day():
+        return "day"
+    @staticmethod
+    def week():
+        return "week"
+    @staticmethod
+    def month():
+        return "month"
+    @staticmethod
+    def year():
+        return "year"
+    @staticmethod
+    def all():
+        return "all"
+
+class SortType():
+    """
+        Please note that timeframe is only used for controversial and top sorting methods
+    """
+    @staticmethod
+    def hot(reddit, subreddit, timeframe, limit):
+        return reddit.subreddit(subreddit).hot(limit=limit)
+    @staticmethod
+    def new(reddit, subreddit, timeframe, limit):
+        return reddit.subreddit(subreddit).new(limit=limit)
+    @staticmethod
+    def controversial(reddit, subreddit, timeframe, limit):
+        return reddit.subreddit(subreddit).controversial(timeframe, limit=limit)
+    @staticmethod
+    def top(reddit, subreddit, timeframe, limit):
+        return reddit.subreddit(subreddit).top(timeframe, limit=limit)
+
+class DataCollector:
+    reddit = 0
+    def __init__ (self, redditobject):
+        self.reddit = redditobject
+
+    def collectPostData(self, sorttype, sub, timeframe, limit):
+        for submission in sorttype(self.reddit, sub, timeframe, limit):
+            print(submission.title)
+    def collectSinglePost(self, postId):
+        submission = self.reddit.submission(id=postId)
+        print(submission.title)
+
+reddit = praw.Reddit(client_id='0Gw8-EwymQ-fig',
+                     client_secret='Y5kigBf4MmZs-u2m3lh6ZvFjclg',
+                     user_agent='my user agent (test)')
+yeettest = DataCollector(reddit)
+yeettest.collectPostData(sorttype=SortType.hot, sub='all', timeframe=SortTime.week, limit=25)
